187f8e870a5568c2c4f0e80336b4adc6c0bccc8d
Xaver <xdxaverdrabik@gmail.com>
2023-03-19 10:36:48 +0100

    st

--- main.cpp
+++ main.cpp
@@ -1,6 +1,57 @@
 #include <iostream>
 
+struct stock{
+    std::string name;
+    float open[30];
+    float high[30];
+    float low[30];
+    float close[30];
+    float adjClose[30];
+    float volume[30];
+};
+
+int hash(int k, int p){
+    return k%p;
+}//done
+
+void add(){
+
+}
+
+void del(){
+
+}
+
+void import(){
+
+}
+
+void search(){
+
+}
+
+void plot(){
+
+}
+
+void save(){
+
+}
+
+void load(){
+
+}
+
+void quit(){
+
+}
+
 int main() {
+    int p=17;
+    stock stocks[p];
+
+
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
