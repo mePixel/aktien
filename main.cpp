ec0c5c4ed7e62ad7160e5c3d936a361f15c3d529
Xaver <xdxaverdrabik@gmail.com>
2023-03-19 11:11:26 +0100

    st

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <string>
+#include <list>
 
 struct stock{
     std::string name;
+    std::string wkn;
+    std::string shorthand;
     float open[30];
     float high[30];
     float low[30];
@@ -15,8 +19,19 @@ int hash(int k, int p){
 }//done
 
 
-void add(){
+stock add(){
+    stock stocks;
 
+    std::cout << "Please input the name of the stock: ";
+    std::cin >> stocks.name;
+
+    std::cout << "Please input the wkn of the stock: ";
+    std::cin >> stocks.wkn;
+
+    std::cout << "Please input the shorthand of the stock: ";
+    std::cin >> stocks.shorthand;
+
+    return stocks;
 }
 
 void del(){
@@ -50,16 +65,22 @@ void quit(bool& run){
 int main() {
     bool run=true;
     int p=17;
-    stock stocks[p];
+
+    std::list<stock> stocks[p];
     std::string input;
 
     while (run){
+        std::cout << "what do you want to do?" << std::endl;
+
         std::cin >> input;
 
         if (input == "q"){
             quit(run);
         }else if (input == "a"){
-            add();
+            stock tmp = add();
+            int pos = hash(std::stoi(tmp.name), p);
+            stocks[pos].insert(tmp);
+            //add element tmp to linked list and insert it to the stocks array at hashed position
         }else if (input == "d"){
             del();
         }else if (input == "i"){
